1f4f92978923d7b093dbc770aa9c5dc4c398c414
Xavier Villelégier <x.villelegier@gmail.com>
2020-02-25 19:54:29 +0100

    Update issue

--- screens/Issue2233.js
+++ screens/Issue2233.js
@@ -1,15 +1,36 @@
 import * as React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
-import { Button, Avatar } from "react-native-elements";
+import { Input, Avatar } from "react-native-elements";
+
+const initialState = {
+  firstname: "Salut",
+  profileImg: {
+    uri:
+      "https://flyinryanhawks.org/wp-content/uploads/2016/08/profile-placeholder.png"
+  }
+};
 
 // https://github.com/react-native-elements/react-native-elements/issues/2233
 export default function Issue2233() {
-  const AVATAR_URI =
-    "https://flyinryanhawks.org/wp-content/uploads/2016/08/profile-placeholder.png";
+  const [profile, setProfile] = React.useState(initialState);
+  console.log(profile)
   return (
     <View style={styles.container}>
-      <Avatar source={{ uri: AVATAR_URI }} rounded size="large" />
+      <Avatar
+        size={"xlarge"}
+        rounded
+        source={profile.profileImg}
+        showEditButton={true}
+      />
+      <Input
+        label={"firstname"}
+        // placeholder="Prénom"
+        autoCapitalize="none"
+        //editable={true}
+        onChangeText={value => setProfile({ ...profile, firstname: value })}
+        value={profile.firstname}
+      />
     </View>
   );
 }
